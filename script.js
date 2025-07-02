826f1ee88d5459294c1aa0f435bedd44244c506c
biazinha2 <145567993+biazinha2@users.noreply.github.com>
2025-07-02 11:29:08 -0300

    script

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 5;
+let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('.forca');
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
